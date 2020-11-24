5304b586173d88491b19f7d3b7525c15355c05cc
jesudornenkrone <christian.huebert@segmeno.de>
2020-11-24 16:30:15 +0100

    made SortEntry public

--- src/main/java/com/segmeno/kodo/transport/Sort.java
+++ src/main/java/com/segmeno/kodo/transport/Sort.java
@@ -18,17 +18,17 @@ public class Sort {
 	 * @param fieldName
 	 * @param sortDirection
 	 */
-	public Sort(String fieldName, SortDirection sortDirection) {
+	public Sort(final String fieldName, final SortDirection sortDirection) {
 		sortFields.add(new SortEntry(fieldName, sortDirection));
 		buildStmt();
 	}
 	
-	public Sort(ArrayList<SortEntry> sortFields) {
+	public Sort(final ArrayList<SortEntry> sortFields) {
 		this.sortFields = sortFields;
 		buildStmt();
 	}
 	
-	public Sort(SortEntry entry) {
+	public Sort(final SortEntry entry) {
 		sortFields.add(entry);
 		buildStmt();
 	}
@@ -40,12 +40,12 @@ public class Sort {
 		sortStmt.setLength(0);
 		sortStmt.append(" ORDER BY");
 		
-		for (SortEntry entry : sortFields) {
+		for (final SortEntry entry : sortFields) {
 			sortStmt.append(" ").append(entry.fieldName).append(" ").append(entry.sortDirection);
 		}
 	}
 	
-	public void addSortField(String fieldName, SortDirection sortDirection) {
+	public void addSortField(final String fieldName, final SortDirection sortDirection) {
 		sortFields.add(new SortEntry(fieldName, sortDirection));
 	}
 	
@@ -53,16 +53,16 @@ public class Sort {
 		return sortFields;
 	}
 	
-	public void setSortFields(ArrayList<SortEntry> sortFields) {
+	public void setSortFields(final ArrayList<SortEntry> sortFields) {
 		this.sortFields = sortFields;
 		buildStmt();
 	}
 
-	class SortEntry {
+	public static class SortEntry {
 		private String fieldName;
 		private SortDirection sortDirection;
 		
-		public SortEntry(String fieldName, SortDirection sortDirection) {
+		public SortEntry(final String fieldName, final SortDirection sortDirection) {
 			this.fieldName = fieldName;
 			this.sortDirection = sortDirection;
 		}
@@ -70,13 +70,13 @@ public class Sort {
 		public String getFieldName() {
 			return fieldName;
 		}
-		public void setFieldName(String fieldName) {
+		public void setFieldName(final String fieldName) {
 			this.fieldName = fieldName;
 		}
 		public SortDirection getSortDirection() {
 			return sortDirection;
 		}
-		public void setSortDirection(SortDirection sortDirection) {
+		public void setSortDirection(final SortDirection sortDirection) {
 			this.sortDirection = sortDirection;
 		}		
 	}
@@ -85,5 +85,4 @@ public class Sort {
 	public String toString() {
 		return sortStmt.toString();
 	}
-	
 }
