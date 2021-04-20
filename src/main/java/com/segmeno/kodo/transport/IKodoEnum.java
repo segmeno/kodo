684126e04a723c0d842642e055f51b1c5e24f4f8
jesudornenkrone <18657428+jesudornenkrone@users.noreply.github.com>
2021-04-20 14:24:38 +0200

    support serialization of enums using jackson

--- src/main/java/com/segmeno/kodo/transport/IKodoEnum.java
+++ src/main/java/com/segmeno/kodo/transport/IKodoEnum.java
@@ -1,12 +1,15 @@
 /**
- * 
+ *
  */
 package com.segmeno.kodo.transport;
+
+import com.fasterxml.jackson.annotation.JsonValue;
 
 /**
  * @author C.Huebert
  *
  */
 public interface IKodoEnum{
+    @JsonValue
 	public String getValue();
 }
